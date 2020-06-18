c906e075d192b7156471b920ed29f2d7068739ae
Andrew St Clair <andrew_stclair95@hotmail.com>
2020-06-18 21:31:04 +1000

    Add testing script

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set variables
+pwd=$(pwd)
+upperdir=${pwd}/testresults
+workdir=/tmp/workdir
+testroot=${pwd}/testroot
+
+# Clean and setup testing directories
+sudo rm -rf $upperdir
+sudo rm -rf $workdir
+sudo rm -rf $testroot
+mkdir -p $upperdir
+mkdir -p $workdir
+mkdir -p $testroot
+
+# Mount fuse-overlayfs
+sudo fuse-overlayfs -o rw,lowerdir=/,upperdir=$upperdir,workdir=$workdir $testroot
+
+# Copy and Test script
+cp auto-steamcmd.sh $testroot/auto-steamcmd.sh
+sudo chroot $testroot /bin/bash -c /auto-steamcmd.sh
+
+# Unmount fuse-overlayfs
+sudo umount -t fuse-overlayfs $testroot
+
+# Display results
+echo "Results can be found in $upperdir"
